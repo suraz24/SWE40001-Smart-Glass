db7f7c19680a75286c5b573456ec037dd1729c22
Shenal <nirushka.samarasinghe@gmail.com>
2018-09-20 14:34:24 +1000

    deleted a section of the code that was not needed ,optimized the code

--- src/object tracking/trackingObjects.js
+++ src/object tracking/trackingObjects.js
@@ -97,33 +97,7 @@ console.log(ypt);
 return [xpt,ypt]    // returns an array with the x and y cordinates 
   };
 
-const getHullDefectVertices = (handContour, hullIndices) => {
-  const defects = handContour.convexityDefects(hullIndices);
-  const handContourPoints = handContour.getPoints();
 
-  // get neighbor defect points of each hull point
-  const hullPointDefectNeighbors = new Map(hullIndices.map(idx => [idx, []]));
-  defects.forEach((defect) => {
-    const startPointIdx = defect.at(0);
-    const endPointIdx = defect.at(1);
-    const defectPointIdx = defect.at(2);
-    hullPointDefectNeighbors.get(startPointIdx).push(defectPointIdx);
-    hullPointDefectNeighbors.get(endPointIdx).push(defectPointIdx);
-  });
-
-  return Array.from(hullPointDefectNeighbors.keys())
-    // only consider hull points that have 2 neighbor defects
-    .filter(hullIndex => hullPointDefectNeighbors.get(hullIndex).length > 1)
-    // return vertex points
-    .map((hullIndex) => {
-      const defectNeighborsIdx = hullPointDefectNeighbors.get(hullIndex);
-      return ({
-        pt: handContourPoints[hullIndex],
-        d1: handContourPoints[defectNeighborsIdx[0]],
-        d2: handContourPoints[defectNeighborsIdx[1]]
-      });
-    });
-};
 
 const filterVerticesByAngle = (vertices, maxAngleDeg) =>
   vertices.filter((v) => {

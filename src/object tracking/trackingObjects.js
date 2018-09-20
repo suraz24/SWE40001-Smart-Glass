0fd35e606f2ddae2de15747f9a332edc515ae817
Ayub <khan.ayub3@hotmail.com>
2018-09-20 18:40:26 +1000

    final edits

--- src/object tracking/trackingObjects.js
+++ src/object tracking/trackingObjects.js
@@ -1,6 +1,5 @@
 const cv = require('../');
 const { grabFrames } = require('./utils');
-
 // segmenting by skin color (has to be adjusted)
 const skinColorUpper = hue => new cv.Vec(hue, 0.8 * 255, 0.9 * 255);
 const skinColorLower = hue => new cv.Vec(hue, 0.1 * 255, 0.6 * 255);
@@ -9,11 +8,9 @@ const makeHandMask = (img) => {
   // filter by skin color
   const imgHLS = img.cvtColor(cv.COLOR_BGR2HLS);
   const rangeMask = imgHLS.inRange(skinColorLower(100), skinColorUpper(180));
-
   // remove noise
   const blurred = rangeMask.blur(new cv.Size(10, 10));
   const thresholded = blurred.threshold(200, 255, cv.THRESH_BINARY);
-
   return thresholded;
 };
 
@@ -25,7 +22,7 @@ const getHandContour = (handMask) => {
   return contours.sort((c0, c1) => c1.area - c0.area)[0];
 };
 
-  const getObjectCenter = (contour) => {
+const getObjectCenter = (contour) => {
   // get hull indices and hull points
   const hullIndices = contour.convexHullIndices();
   const contourPoints = contour.getPoints();
@@ -34,10 +31,9 @@ const getHandContour = (handMask) => {
     contourIdx: idx
   }));
   const hullPoints = hullPointsWithIdx.map(ptWithIdx => ptWithIdx.pt);
-  
   // get the x and y values of the center of the object 
   var xpt = 0;		//contains the x cordinates
-  var ypt = 0		// contains the y cordinates
+  var ypt = 0;		// contains the y cordinates
   for (var i=0;i<hullPoints.length;i++)
   {
 	  xpt = xpt+(hullPoints[i].x)
@@ -45,37 +41,23 @@ const getHandContour = (handMask) => {
   }
   xpt =  (xpt/(hullPoints.length)).toFixed(0)
   ypt = (ypt/(hullPoints.length)).toFixed(0)
-console.log(xpt);
-console.log(ypt);
-
-return [xpt,ypt]    // returns an array with the x and y cordinates 
+  console.log(xpt);
+  console.log(ypt);
+  return [xpt,ypt]    // returns an array with the x and y cordinates 
   };
-
-
-
+  
 const blue = new cv.Vec(255, 0, 0);
-
-
 // main
 const delay = 20;
 grabFrames('../data/example5.mp4', delay, (frame) => {
   const resizedImg = frame.resizeToMax(640);
-
   const handMask = makeHandMask(resizedImg);
   const handContour = getHandContour(handMask);
   if (!handContour) {
     return;
   }
-  
   const maxPointDist = 25;
-  
   const objectCenter = getObjectCenter(handContour);
-
-  // get defect points of hull to contour and return vertices
-  // of each hull point to its defect points
-  // fingertip points are those which have a sharp angle to its defect points
- 
-
   const result = resizedImg.copy();
   // draw bounding box and center line
   resizedImg.drawContours(
@@ -83,24 +65,7 @@ grabFrames('../data/example5.mp4', delay, (frame) => {
     blue,
     { thickness: 2 }
   );
-  
   // display detection result
-  const numFingersUp = verticesWithValidAngle.length;
-  result.drawRectangle(
-    new cv.Point(10, 10),
-    new cv.Point(70, 70),
-    { color: green, thickness: 2 }
-  );
-
-  const fontScale = 2;
-  result.putText(
-    String(numFingersUp),
-    new cv.Point(20, 60),
-    cv.FONT_ITALIC,
-    fontScale,
-    { color: green, thickness: 2 }
-  );
-  
   const { rows, cols } = result;
   const sideBySide = new cv.Mat(rows, cols * 2, cv.CV_8UC3);
   result.copyTo(sideBySide.getRegion(new cv.Rect(0, 0, cols, rows)));

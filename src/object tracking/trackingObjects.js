7ea275b2343c74036ac91dfa9f9589e19fc4c9ec
Dineth <dinethgun456@gmail.com>
2018-09-20 14:38:06 +1000

    Removed Vertices Filter

--- src/object tracking/trackingObjects.js
+++ src/object tracking/trackingObjects.js
@@ -125,17 +125,8 @@ const getHullDefectVertices = (handContour, hullIndices) => {
     });
 };
 
-const filterVerticesByAngle = (vertices, maxAngleDeg) =>
-  vertices.filter((v) => {
-    const sq = x => x * x;
-    const a = v.d1.sub(v.d2).norm();
-    const b = v.pt.sub(v.d1).norm();
-    const c = v.pt.sub(v.d2).norm();
-    const angleDeg = Math.acos(((sq(b) + sq(c)) - sq(a)) / (2 * b * c)) * (180 / Math.PI);
-    return angleDeg < maxAngleDeg;
-  });
-
 const blue = new cv.Vec(255, 0, 0);
+
 
 // main
 const delay = 20;
